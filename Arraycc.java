da29723c479ca644f1f23086c4fbda9d685be333
Soham Shendre <87564877+sohams4@users.noreply.github.com>
2022-10-04 21:40:14 +0530

    Create Arraycc.java

---
+++ Arraycc.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Arraycc {
+  public static int getLargest(int numbers[]) {
+    int largest = Integer.MIN_VALUE;
+     for(int i=0; i<numbers.length; i++ ){
+        if(largest < numbers[i]){
+            largest = numbers[i];
+
+        }
+    }
+    return largest;
+   }
+    public static void main(String[] args) {
+        int numbers[] = {41, 62, 86, 23, 35} ;
+      System.out.println("Largest value is "+ getLargest(numbers) );
+      }
+}
