55fa6579d0b102c187b3383b9e22d5753cdb87a8
Soham Shendre <87564877+sohams4@users.noreply.github.com>
2024-05-29 16:48:07 +0530

    Insertion.java

--- Insertion.java
+++ Insertion.java
@@ -7,6 +7,7 @@ public class Insertion {
             int temp =arr[i];
             int j =i-1;
             while(j>0 && arr[j]>temp){
+                
                 arr[j+1]=arr[j];
                 j--;
             }
